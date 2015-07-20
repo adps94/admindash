eab24eec8d65ed2c0f7318fbd4cd830266f354dd
Pranav Raj S <pranavrajs@gmail.com>
2015-07-20 22:40:51 +0530

    Edits in index

--- assets/js/angular/todo.js
+++ assets/js/angular/todo.js
@@ -1,32 +1,14 @@
-
-
-    function TodoCtrl($scope) {
-    $scope.todos = [
-    {text:'Angular Application', done:true},
-	{text:'In eget pretium nisi', done:true},
-	{text:'Developed by Akshay', done:true},
-	{text:'Sed ac scelerisque ipsum', done:true},
-    {text:'build an angular app', done:false}];
-     
-    $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-    };
-     
-    $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-    count += todo.done ? 0 : 1;
-    });
-    return count;
-    };
-     
-    $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-    if (!todo.done) $scope.todos.push(todo);
-    });
-    };
+    function DashCtrl($scope, $http) {
+        $http({
+                method: 'GET',
+                url: 'https://api.parse.com/1/classes/userVisit',
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Parse-Application-Id": "52r6nwCF2fHdqy7UJ5nugOJr5r09ckEoEGp9drWU",
+                    "X-Parse-REST-API-Key": "rfs2A2Om3Q0erGIc4suh6wBzbdphGQnFytmFMFMt"
+                }
+            })
+            .success(function(successData) {
+                $scope.visitorList = successData.results;
+            });
     }
-
