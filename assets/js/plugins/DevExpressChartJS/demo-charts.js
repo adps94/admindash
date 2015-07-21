8b244561e23a5e3d4214bd9b23ab3bc00f7df162
rohanreji93 <rohanreji93@gmail.com>
2015-07-21 11:23:11 +0530

    pie-chart arranged

--- assets/js/plugins/DevExpressChartJS/demo-charts.js
+++ assets/js/plugins/DevExpressChartJS/demo-charts.js
@@ -388,23 +388,21 @@ Pie Chart
 *******************************************/
 
 $("#pie-chart").dxPieChart({
-    dataSource: [
-				  { country: "USA", medals: 110 },
-				  { country: "China", medals: 100 },
-				  { country: "Russia", medals: 72 },
-				  { country: "Britain", medals: 47 },
-				  { country: "Australia", medals: 46 },
-				  { country: "Germany", medals: 41 },
-				  { country: "France", medals: 40 },
-				  { country: "South Korea", medals: 31 }
-				],
+     dataSource: [
+                  { visitors: "delegates", count: 110 },
+                  { visitors: "meeting", count: 100 },
+                  { visitors: "interview", count: 72 },
+                  { visitors: "guests", count: 47 },
+                  { visitors: "courrier", count: 46 },
+                  { visitors: "other", count: 41 }
+                ],
     legend: {
-		visible: false
+        visible: false
     },
-	palette: ["#6bb802", "#7c37c3", "#0861ce", "#fbd005", "#4fcdfc", "#00b19d", "#ff6264"],
+    palette: ["#6bb802", "#7c37c3", "#0861ce", "#fbd005", "#4fcdfc", "#00b19d"],
     series: [{
-        argumentField: "country",
-        valueField: "medals",
+        argumentField: "visitors",
+        valueField: "count",
         label: {
             visible: true,
             font: {
@@ -416,7 +414,8 @@ $("#pie-chart").dxPieChart({
             },
             position: "columns",
             customizeText: function(arg) {
-                return arg.valueText + " ( " + arg.percentText + ")";
+               
+                return arg.argument ;
             }
         }
     }]
